ef86754c5db7d46d5a236a6d45d49a9485c087a0
Chayoung You <yousbe@gmail.com>
2015-11-03 23:26:07 +0900

    Implement parsing instruction

--- lab2/parse.c
+++ lab2/parse.c
@@ -26,9 +26,50 @@ int data_size;
 /*                                                            */
 /**************************************************************/
 instruction parsing_instr(const char *buffer, const int index) {
-    instruction instr = {0, };
+    instruction instr = INST_INFO[index];
+    uint32_t inst_binary = fromBinary((char *) buffer);
 
-    /* Your code */
+    instr.opcode = inst_binary >> 26;
+    switch (instr.opcode) {
+        // R-type
+        case 0:
+            instr.func_code = inst_binary & 0x3F;
+            switch (instr.func_code) {
+                // JR
+                case 8:
+                    instr.r_t.r_i.rs = (inst_binary >> 21) & 0x1F;
+                    break;
+                // SLL
+                case 0:
+                // SRL
+                case 2:
+                    instr.r_t.r_i.rt = (inst_binary >> 16) & 0x1F;
+                    instr.r_t.r_i.r_i.r.rd = (inst_binary >> 11) & 0x1F;
+                    instr.r_t.r_i.r_i.r.shamt = (inst_binary >> 6) & 0x1F;
+                    break;
+                // ADDU, AND, NOR, OR, SLTU, SUBU
+                default:
+                    instr.r_t.r_i.rs = (inst_binary >> 21) & 0x1F;
+                    instr.r_t.r_i.rt = (inst_binary >> 16) & 0x1F;
+                    instr.r_t.r_i.r_i.r.rd = (inst_binary >> 11) & 0x1F;
+                    break;
+            }
+            break;
+        // J-type
+        case 2:
+        case 3:
+            instr.r_t.target = inst_binary & 0x3FFFFFF;
+            break;
+        // I-type
+        default:
+            instr.r_t.r_i.rt = (inst_binary >> 16) & 0x1F;
+            instr.r_t.r_i.r_i.imm = inst_binary & 0xFFFF;
+            // Not LUI
+            if (instr.opcode != 15) {
+                instr.r_t.r_i.rs = (inst_binary >> 21) & 0x1F;
+            }
+            break;
+    }
 
     return instr;
 }
@@ -45,7 +86,5 @@ instruction parsing_instr(const char *buffer, const int index) {
 /*                                                            */
 /**************************************************************/
 void parsing_data(const char *buffer, const int index) {
-
-    /* Your code */
-
+    mem_write_32(MEM_DATA_START + index, fromBinary((char *) buffer));
 }
