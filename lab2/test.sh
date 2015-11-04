ef9bd49ba1fdd2196b646c079f69510c071428ae
Chayoung You <yousbe@gmail.com>
2015-11-04 13:43:36 +0900

    Add a simple test script

---
+++ lab2/test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+make
+
+function res_print()
+{
+  if [ $? -eq 0 ]; then
+    echo 'Pass'
+  else
+    echo 'Fail'
+  fi
+}
+
+for FILENAME in sample_input/*.o
+do
+  echo "Testing with ${FILENAME} ..."
+
+  echo -n -e "No option: \t\t\t"
+  diff -u <(./cs311test $FILENAME) <(./cs311em $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-d: \t\t\t\t"
+  diff -u <(./cs311test -d $FILENAME) <(./cs311em -d $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-d -m 0x100000:0x100100: \t"
+  diff -u <(./cs311test -d -m 0x100000:0x100100 $FILENAME) \
+    <(./cs311em -d -m 0x100000:0x100100 $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-m 0x400000:0x400100: \t\t"
+  diff -u <(./cs311test -m 0x400000:0x400100 $FILENAME) \
+    <(./cs311em -m 0x400000:0x400100 $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-m 0x10000000:0x10000100: \t"
+  diff -u <(./cs311test -m 0x10000000:0x10000100 $FILENAME) \
+    <(./cs311em -m 0x10000000:0x10000100 $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-d -m 0x400000:0x400100: \t"
+  diff -u <(./cs311test -d -m 0x400000:0x400100 $FILENAME) \
+    <(./cs311em -d -m 0x400000:0x400100 $FILENAME) > /dev/null
+  res_print
+
+  echo -n -e "-d -m 0x10000000:0x10000100: \t"
+  diff -u <(./cs311test -d -m 0x10000000:0x10000100 $FILENAME) \
+    <(./cs311em -d -m 0x10000000:0x10000100 $FILENAME) > /dev/null
+  res_print
+  echo
+done
