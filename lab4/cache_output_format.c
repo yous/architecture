d162163b7f7bffa1263d9e3e7167b090065968e2
Chayoung You <yousbe@gmail.com>
2015-12-20 17:46:54 +0900

    Clean up

--- lab4/cache_output_format.c
+++ lab4/cache_output_format.c
@@ -8,53 +8,51 @@
 /*                                                             */
 /* Procedure : cdump                                           */
 /*                                                             */
-/* Purpose   : Dump cache configuration                        */   
+/* Purpose   : Dump cache configuration                        */
 /*                                                             */
 /***************************************************************/
-void cdump(int capacity, int assoc, int blocksize){
-
-	printf("Cache Configuration:\n");
-    	printf("-------------------------------------\n");
-	printf("Capacity: %dB\n", capacity);
-	printf("Associativity: %dway\n", assoc);
-	printf("Block Size: %dB\n", blocksize);
-	printf("\n");
+void cdump(int capacity, int assoc, int blocksize) {
+    printf("Cache Configuration:\n");
+    printf("-------------------------------------\n");
+    printf("Capacity: %dB\n", capacity);
+    printf("Associativity: %dway\n", assoc);
+    printf("Block Size: %dB\n", blocksize);
+    printf("\n");
 }
 
 /***************************************************************/
 /*                                                             */
 /* Procedure : sdump                                           */
 /*                                                             */
-/* Purpose   : Dump cache stat		                       */   
+/* Purpose   : Dump cache stat                                 */
 /*                                                             */
 /***************************************************************/
 void sdump(int total_reads, int total_writes, int write_backs,
-	int reads_hits, int write_hits, int reads_misses, int write_misses) {
-	printf("Cache Stat:\n");
-    	printf("-------------------------------------\n");
-	printf("Total reads: %d\n", total_reads);
-	printf("Total writes: %d\n", total_writes);
-	printf("Write-backs: %d\n", write_backs);
-	printf("Read hits: %d\n", reads_hits);
-	printf("Write hits: %d\n", write_hits);
-	printf("Read misses: %d\n", reads_misses);
-	printf("Write misses: %d\n", write_misses);
-	printf("\n");
+        int reads_hits, int write_hits, int reads_misses, int write_misses) {
+    printf("Cache Stat:\n");
+    printf("-------------------------------------\n");
+    printf("Total reads: %d\n", total_reads);
+    printf("Total writes: %d\n", total_writes);
+    printf("Write-backs: %d\n", write_backs);
+    printf("Read hits: %d\n", reads_hits);
+    printf("Write hits: %d\n", write_hits);
+    printf("Read misses: %d\n", reads_misses);
+    printf("Write misses: %d\n", write_misses);
+    printf("\n");
 }
-
 
 /***************************************************************/
 /*                                                             */
 /* Procedure : xdump                                           */
 /*                                                             */
-/* Purpose   : Dump current cache state                        */ 
-/* 							       */
-/* Cache Design						       */
-/*  							       */
-/* 	    cache[set][assoc][word per block]		       */
-/*      						       */
-/*      						       */
-/*       ----------------------------------------	       */
+/* Purpose   : Dump current cache state                        */
+/*                                                             */
+/* Cache Design                                                */
+/*                                                             */
+/*          cache[set][assoc][word per block]                  */
+/*                                                             */
+/*                                                             */
+/*       ----------------------------------------              */
 /*       I        I  way0  I  way1  I  way2  I                 */
 /*       ----------------------------------------              */
 /*       I        I  word0 I  word0 I  word0 I                 */
@@ -67,68 +65,59 @@ void sdump(int total_reads, int total_writes, int write_backs,
 /*       I        I  word2 I  word2 I  word2 I                 */
 /*       I        I  word3 I  word3 I  word3 I                 */
 /*       ----------------------------------------              */
-/*      						       */
+/*                                                             */
 /*                                                             */
 /***************************************************************/
-void xdump(int set, int way, uint32_t** cache)
-{
-	int i,j,k = 0;
+void xdump(int set, int way, uint32_t **cache) {
+    int i, j, k = 0;
 
-	printf("Cache Content:\n");
-    	printf("-------------------------------------\n");
-	for(i = 0; i < way;i++)
-	{
-		if(i == 0)
-		{
-			printf("    ");
-		}
-		printf("      WAY[%d]",i);
-	}
-	printf("\n");
+    printf("Cache Content:\n");
+    printf("-------------------------------------\n");
+    for (i = 0; i < way; i++) {
+        if (i == 0) {
+            printf("    ");
+        }
+        printf("      WAY[%d]", i);
+    }
+    printf("\n");
 
-	for(i = 0 ; i < set;i++)
-	{
-		printf("SET[%d]:   ",i);
-		for(j = 0; j < way;j++)
-		{
-			if(k != 0 && j == 0)
-			{
-				printf("          ");
-			}
-			printf("0x%08x  ", cache[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
+    for (i = 0; i < set; i++) {
+        printf("SET[%d]:   ", i);
+        for (j = 0; j < way; j++) {
+            if (k != 0 && j == 0) {
+                printf("          ");
+            }
+            printf("0x%08x  ", cache[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
+int main(int argc, char *argv[]) {
+    uint32_t **cache;
+    int i, j, k;
+    int capacity = 256;
+    int way = 4;
+    int blocksize = 8;
+    int set = capacity / way / blocksize;
+    int words = blocksize / BYTES_PER_WORD;
 
+    // allocate
+    cache = (uint32_t **) malloc(sizeof(uint32_t *) * set);
+    for (i = 0; i < set; i++) {
+        cache[i] = (uint32_t *) malloc(sizeof(uint32_t) * way);
+    }
+    for (i = 0; i < set; i++) {
+        for (j = 0; j < way; j++) {
+            cache[i][j] = 0x0;
+        }
+    }
 
+    // test example
+    cdump(capacity, way, blocksize);
+    sdump(0, 0, 0, 0, 0, 0, 0);
+    xdump(set, way, cache);
 
-int main(int argc, char *argv[]) {                              
-
-	uint32_t** cache;
-	int i, j, k;	
-	int capacity = 256;
-	int way = 4;
-	int blocksize = 8;
-	int set = capacity/way/blocksize;
-	int words = blocksize / BYTES_PER_WORD;	
-
-	// allocate
-	cache = (uint32_t**) malloc (sizeof(uint32_t*) * set);
-	for(i = 0; i < set; i++) {
-		cache[i] = (uint32_t*) malloc(sizeof(uint32_t) * way);
-	}
-	for(i = 0; i < set; i++) {
-		for(j = 0; j < way; j ++) 
-			cache[i][j] = 0x0;
-	}
-
-	// test example
-    	cdump(capacity, way, blocksize);
-    	sdump(0, 0, 0, 0, 0, 0, 0); 
-    	xdump(set, way, cache);
-
-    	return 0;
+    return 0;
 }
